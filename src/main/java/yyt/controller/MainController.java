a8bd48b78991fb0fd6a0ecf46fdd2268dca97545
494003007 <c494003007@qq.com>
2016-12-08 16:04:52 +0800

    jhj

--- src/main/java/yyt/controller/MainController.java
+++ src/main/java/yyt/controller/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class MainController  {
+    int a;
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String index(){
         System.out.println("#######################  index  #######################");
--- src/mybatis_generate/mybatis_generate.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd /home/hadoop/IdeaProjects/czy/src/mybatis_generate
-java -jar mybatis-generator-core-1.3.2.jar -configfile generatorConfig.xml -overwrite
