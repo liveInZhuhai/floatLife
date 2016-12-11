ab09afb606c71709859d90817c57ecb7e9fe0fc1
hzd <hzd138@qq.com>
2016-12-11 23:37:00 +0800

    =

---
+++ src/dataservice/test.java
@@ -0,0 +1,10 @@
+package dataservice;
+
+/**
+ * Created by D on 2016/12/11.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
