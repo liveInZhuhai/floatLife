c210c1272aa8709bd7fcb5c11fbff4c646e8653b
494003007 <c494003007@qq.com>
2016-12-20 17:20:10 +0800

    URLSMap

--- src/service/controller/ControllerBase.java
+++ src/service/controller/ControllerBase.java
@@ -1,9 +1,14 @@
 package service.controller;
 
+import com.alibaba.fastjson.JSON;
+
+import java.util.Map;
+
 /**
  * Created by Admini on 2016/12/20.
  */
 public abstract class ControllerBase {
-
-
+    public byte[] getJson(Map map){
+        return ("{\"c2dictionary\":true,\"data\":"+ JSON.toJSONString(map) + "}").getBytes();
+    }
 }
